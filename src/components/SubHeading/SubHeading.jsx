2995bf21a088b1a5a352be7ac8f6082a627249a0
Wilson Pinto <wilsonpinto88@gmail.com>
2023-03-02 11:28:04 +0000

    added the Heading and About sections

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
+import { images } from "../../constants";
+
+const SubHeading = ({ title }) => (
+  <Fragment>
+    <div style={{ marginBottom: "1rem" }}>
+      <p className="p__cormorant">{title}</p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
+    </div>
+  </Fragment>
 );
 
 export default SubHeading;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { SubHeading } from "../../components";
 
-import './Header.css';
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <Fragment>
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app__header-h1">The Key to Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: "2rem 0" }}>
+          Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+          volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+          aliquam amet tellus
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
+      </div>
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header" />
+      </div>
+    </div>
+  </Fragment>
 );
 
 export default Header;
