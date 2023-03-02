e41065728a7dd5b53b97d73908dc5e32795dcfe8
Wilson Pinto <wilsonpinto88@gmail.com>
2023-03-02 16:26:59 +0000

    added the Chef and Intro sections

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Fragment, useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import './Intro.css';
+import { meal } from "../../constants";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = React.useRef();
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="app__video">
+        <video
+          src={meal}
+          ref={vidRef}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+        />
+        <div className="app__video-overlay flex__center">
+          <div
+            className="app__video-overlay_circle flex__center"
+            onClick={handleVideo}
+          >
+            {playVideo ? (
+              <BsPauseFill color="#fff" fontSize={30} />
+            ) : (
+              <BsFillPlayFill color="#fff" fontSize={30} />
+            )}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Intro;
