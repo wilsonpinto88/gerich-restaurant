6d558d456332cc91c4e284cfb502d698e6670b41
Wilson Pinto <wilsonpinto88@gmail.com>
2023-03-02 12:59:56 +0000

    added the Menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
+const MenuItem = ({ title, price, tags }) => (
+  <Fragment>
+    <div className="app__menuitem">
+      <div className="app__menuitem-head">
+        <div app__menuitem-name>
+          <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+            {title}
+          </p>
+        </div>
+        <div className="app__menuitem-dash" />
+        <div classname="app__menuitem-price">
+          <p className="p__cormorant">{price}</p>
+        </div>
+      </div>
+      <div className="app__menuitem-sub">
+        <p className="p__opensans" style={{ color: "#AAA" }}>
+          {tags}
+        </p>
+      </div>
+    </div>
+  </Fragment>
 );
 
 export default MenuItem;
